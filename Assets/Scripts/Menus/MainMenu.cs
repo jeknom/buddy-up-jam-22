b6a7f2d0be62d26eb8cfe97e2b305bc4892dfa95
DexTG <dextilitygamer@live.dk>
2022-02-09 22:07:37 +0100

    Added main menu

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject creditsMenu;
+    public Slider volSlider;
+    public AudioMixer mainMixer;
+    private float volume;
+
+    void start()
+    {
+        volume = PlayerPrefs.GetFloat("volume");
+        volSlider.value = volume;
+        mainMixer.SetFloat("MainVolume", volume);
+
+    }
+
+
+
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Settings()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        creditsMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void SetVolume(float vol)
+    {
+        mainMixer.SetFloat("MainVolume", vol);
+        PlayerPrefs.SetFloat("volume",vol);
+    }
+
+}
