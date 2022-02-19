353f03c5911497eed912e5c936ef0b07cd61b36a
Cylpen <Cylpen@gmail.com>
2022-02-19 16:40:04 +0100

    Added breakable log

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -3,38 +3,8 @@ using UnityEngine;
 
 namespace Game
 {
-    [RequireComponent(typeof(SpriteRenderer))]
     public class Breakable : MonoBehaviour
     {
-        [Header("Modifiers")]
-        [SerializeField, Range(0.1f, 3f)] float breakDelay;
-
-        SpriteRenderer spriteRenderer;
-
-        void Awake()
-        {
-            this.spriteRenderer = this.GetComponent<SpriteRenderer>();
-        }
-
-        public void Break() => this.StartCoroutine(this.BreakInternal());
-
-        IEnumerator BreakInternal()
-        {
-            var passedDelay = this.breakDelay;
-            while (passedDelay > 0)
-            {
-                var deltaTime = Time.deltaTime;
-                passedDelay -= deltaTime;
-                var percentOfTimePassed = passedDelay / this.breakDelay;
-                var currentColor = this.spriteRenderer.color;
-                currentColor.a = percentOfTimePassed;
-                this.spriteRenderer.color = currentColor;
-
-                yield return new WaitForSeconds(deltaTime);
-            }
-
-
-            Destroy(this.gameObject);
-        }
+        public void Break() => Destroy(this.gameObject);
     }
 }
